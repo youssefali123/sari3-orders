08d6cbdc2039abe0ba537f505243b89112697174
youssefali123 <77068196+youssefali123@users.noreply.github.com>
2026-02-10 19:04:50 +0200

    Add files via upload

--- src/services/send_message.ts
+++ src/services/send_message.ts
@@ -1,18 +1,33 @@
-
-const url = "https://7107.api.greenapi.com/waInstance7107488037/sendMessage/d4c4483bd7074e7e8adf9b124d6ab658562831e4213741fb94";
+// const CHAT_ID = "1170515116";
+const adham_id = "1427721323";
+const token = "bot7591746594:AAGZd2XDfGpJ6oU7iukMP2OiN7-CNeeL-k0";
+// const url = "https://7107.api.greenapi.com/waInstance7107488037/sendMessage/d4c4483bd7074e7e8adf9b124d6ab658562831e4213741fb94";
 
 const sendMessage = async (phone: string, message: string) => {
-    const payload = {
-        chatId: `${phone}@c.us`,
-        message: message
-    }
+    const url = `https://api.telegram.org/${token}/sendMessage`;
+    // const payload = {
+    //     chatId: `${phone}@c.us`,
+    //     message: message
+    // }
     try {
+        // const response = await fetch(url, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(payload),
+        // });
         const response = await fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(payload),
+            body: JSON.stringify({
+                chat_id: adham_id,
+                text: message,
+                parse_mode: "Markdown",
+                
+            }),
         });
         const data = await response.json();
         console.log("message sent")
