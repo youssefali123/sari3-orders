c85a256b026c785a87d72ee4f93dd093d5ff8a77
youssefali123 <77068196+youssefali123@users.noreply.github.com>
2026-04-18 23:16:23 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // import { Toaster } from "./components/ui/toaster";
-import { Toaster as Sonner } from "./components/ui/sonner";
 import { Toaster } from "react-hot-toast";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Dashboard from "./pages/Dashboard";
 import { useEffect } from "react";
 import { fetchMenus } from "./store/menuSlice";
 import { useAppDispatch } from "./hooks/useRedux";
@@ -27,6 +27,9 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/market" element={<Index />} />
+            <Route path="/restaurant" element={<Index />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
