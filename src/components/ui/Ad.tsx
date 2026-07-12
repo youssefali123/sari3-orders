5a1a80db9e202da107c4ccb5c028ed75d5796064
youssefali123 <77068196+youssefali123@users.noreply.github.com>
2026-07-12 03:33:56 +0300

    Add files via upload

--- src/components/ui/Ad.tsx
+++ src/components/ui/Ad.tsx
@@ -45,4 +45,49 @@ const Ad = () => {
   );
 };
 
+export const Ad2 = () => {
+  const adRef = useRef(null);
+
+  useEffect(() => {
+    // Set Adsterra options
+    (window as any).atOptions = {
+      key: "2a6014fc45b670959a6a94c3d8fef5c7",
+      format: "iframe",
+      height: 60,
+      width: 468,
+      params: {},
+    };
+
+    // Create script
+    const script = document.createElement("script");
+    script.src =
+      "https://www.highperformanceformat.com/2a6014fc45b670959a6a94c3d8fef5c7/invoke.js";
+    script.async = true;
+
+    adRef.current.appendChild(script);
+
+    return () => {
+      if (adRef.current) {
+        adRef.current.innerHTML = "";
+      }
+    };
+  }, []);
+
+  return (
+    <div style={{
+      width:"100%",
+      overflow: "hidden"
+    }}>
+    <div
+      
+      ref={adRef}
+      style={{
+        width: "468px",
+        height: "60px",
+      }}
+    />
+    </div>
+  );
+};
+
 export default Ad;
